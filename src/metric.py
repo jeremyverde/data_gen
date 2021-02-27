6ac27986e7a7bc508d5ad04c55b54783e3ffca23
jeremy <jeremygreen27.com>
2021-02-27 21:22:18 +0000

    normal dist values

--- src/metric.py
+++ src/metric.py
@@ -1,6 +1,8 @@
 class Metric:
-    def __init__(self, name, data_type=None, min_val=0, max_val=100):
+    def __init__(self, name, data_type=None, min_val=0, max_val=100, mean=0, sd=0.1):
         self.name = name
         self.data_type = data_type
         self.min_val = min_val
         self.max_val = max_val
+        self.mean = mean
+        self.sd = sd
